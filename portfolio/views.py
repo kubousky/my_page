df042ec9ca3f3972f376c426bb125db25fe27371
kubousky <kparcheta@gmail.com>
2019-10-21 13:41:26 +0200

    recent works almost done_2

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,8 +3,14 @@ from .models import Category, Project
 
 
 def home(request):
-    projects = Project.objects.all()
-    return render(request, "home.html", {'projects': projects})
+    project_1 = Project.objects.get(slug="sport-event")
+    project_2 = Project.objects.get(slug="crypto-bank")
+    project_3 = Project.objects.get(slug="online-shop")
+    project_4 = Project.objects.get(slug="kaprekar-constant")
+    return render(request, "home.html", {'project_1': project_1,
+                                        'project_2': project_2,
+                                        'project_3': project_3,
+                                        'project_4': project_4})
 
 def about(request):
     return render(request, "about.html", {})
