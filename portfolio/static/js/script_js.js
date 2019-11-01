579fcab6dd04450ed0d572fe57b57a77a3897c24
kubousky <kparcheta@gmail.com>
2019-11-01 19:10:57 +0100

    responsive < 1040px

--- portfolio/static/js/script_js.js
+++ portfolio/static/js/script_js.js
@@ -1,5 +1,11 @@
+//* JQuerry iguala height de .c a la de .flex-fill */
 jQuery(function($){
     var mainHeight = $('.flex-fill').height();          
     $('.c').css('height', mainHeight );
 
 });
+// refresh page on browser resize
+$(window).on('resize',function(){location.reload();
+});
+
+//refresh once change page  !!!!
